437102309a3a9796900c1e3eb10c59dfc10e73a1
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-10-31 22:39:04 +0600

    Add program: ascii_to_char.c
    Implemented a C program to display an ASCII value character.

---
+++ ascii_to_char.c
@@ -0,0 +1,17 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: ascii_to_char.c */
+
+#include<stdio.h>
+
+int main(){
+
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("The ASCII character is %c", number);
+
+    return 0;
+}
