9c56cd481e553629481929d38f74505e2545c3fa
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-02 18:33:18 +0600

    Add program: hexadecimal_to_decimal.c
    Implemented a C program to convert a hexadecimal number to its decimal equivalent.

---
+++ hexadecimal_to_decimal.c
@@ -0,0 +1,17 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: hexadecimal_to_decimal.c */
+
+#include<stdio.h>
+
+int main(){
+
+    int number;
+
+    printf("Enter a hexadecimal number: ");
+    scanf("%x", &number);
+
+    printf("Decimal number = %d", number);
+
+    return 0;
+}
