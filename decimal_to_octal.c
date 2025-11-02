4be9e1d987479ad3f45a00edbb61bfbdf9768f96
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-02 18:41:58 +0600

    Add program: decimal_to_octal.c
    Implemented a C program to convert a decimal number to its octal equivalent.

---
+++ decimal_to_octal.c
@@ -0,0 +1,17 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: decimal_to_octal.c */
+
+#include<stdio.h>
+
+int main(){
+
+    int number;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &number);
+
+    printf("Octal number = %o", number);
+
+    return 0;
+}
