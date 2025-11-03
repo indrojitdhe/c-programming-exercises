b953896a3dad915e9630adc408c9c44ff89bfb8e
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-03 18:05:00 +0600

    Add program: size_of_char.c
    Implemented a C program to display the size of a character.

---
+++ size_of_char.c
@@ -0,0 +1,14 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: size_of_char.c */
+
+#include<stdio.h>
+
+int main(){
+
+    char c;
+
+    printf("Char = %d byte", sizeof(c));
+
+    return 0;
+}
