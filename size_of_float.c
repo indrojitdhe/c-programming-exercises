ae95c633c28462c5e1b716ed54a7832f6de0a676
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-03 18:02:43 +0600

    Add program: size_of_float.c
    Implemented a C program to display the size of a float.

---
+++ size_of_float.c
@@ -0,0 +1,14 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: size_of_float.c */
+
+#include<stdio.h>
+
+int main(){
+
+    float f;
+
+    printf("Float = %d bytes", sizeof(f));
+
+    return 0;
+}
