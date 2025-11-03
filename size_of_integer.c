2bb7776b1d14b193e167397edef7d8d09e167a64
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-03 17:59:28 +0600

    Add program: size_of_integer.c
    Implemented a C program to display the size of an integer.

---
+++ size_of_integer.c
@@ -0,0 +1,14 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: size_of_integer.c */
+
+#include<stdio.h>
+
+int main(){
+
+    int i;
+
+    printf("Integer = %d bytes", sizeof(i));
+
+    return 0;
+}
