cfc64a36742a6ec20d6fbb2bd5b6d6fe8e12471d
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-03 18:08:48 +0600

    Add program: size_of_double.c
    Implemented a C program to display the size of a double.

---
+++ size_of_double.c
@@ -0,0 +1,14 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: size_of_double.c */
+
+#include<stdio.h>
+
+int main(){
+
+    double d;
+
+    printf("Double = %d bytes", sizeof(d));
+
+    return 0;
+}
