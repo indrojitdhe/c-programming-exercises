bd8491488abb68f5de2e7aa0db4bd924556ddd98
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-11-04 22:31:36 +0600

    Add program: check_leap_year.c
    Implemented a C program to check whether a given year is a leap year or not.

---
+++ check_leap_year.c
@@ -0,0 +1,28 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: check_leap_year.c */
+
+#include<stdio.h>
+
+int main(){
+
+    int year;
+
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if(year%400 == 0){
+
+        printf("Leap Year");
+    }
+    else if(year%4 == 0 && year%100 != 0){
+
+        printf("Leap Year");
+    }
+    else{
+
+        printf("Not a Leap Year");
+    }
+
+    return 0;
+}
