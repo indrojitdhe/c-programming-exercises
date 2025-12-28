060cf44280dd5874fb93c5189ef58e52bf7460e0
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2025-12-29 03:21:28 +0600

    Add program: display_name_using_function.c
    Implemented a C program to display a name using a function.

---
+++ display_name_using_function.c
@@ -0,0 +1,23 @@
+/* Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: display_name_using_function.c */
+
+#include<stdio.h>
+
+void displayName(); /* Function Prototype*/
+
+int main(){
+
+    displayName(); /* Function Call*/
+
+    return 0;
+}
+
+/* Function Definition*/
+
+void displayName(){                  
+
+     printf("INDROJIT DHE SHAON");
+
+}
+
